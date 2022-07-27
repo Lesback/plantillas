afd516c1aa090c8b10cc689c36dc2b3cd910c696
Lesback <109242503+Lesback@users.noreply.github.com>
2022-07-27 17:28:50 +0200

    Update plantilla-comando.js

--- plantilla-comando.js
+++ plantilla-comando.js
@@ -1,8 +1,8 @@
 const { EmbedBuilder } = require('discord.js')
 
 module.exports = {
-  name: 'nombre del comando'
-  alias ['alias del comando', 'puede haber mas de un alias xd']
+  name: 'nombre del comando',
+  alias ['alias del comando', 'puede haber mas de un alias xd'],
   
  run (client, message, args) {
   //código del comando
